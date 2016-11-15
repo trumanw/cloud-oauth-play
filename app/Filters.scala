da582d2952fdd0481f95ef78f1cbee485994b5d4
truman-misfit <truman@misfit.com>
2016-11-15 19:56:18 +0800

    Add logging, gzip and cors filters.

--- app/Filters.scala
+++ app/Filters.scala
@@ -1,9 +1,11 @@
 import javax.inject._
 import play.api._
 import play.api.http.HttpFilters
+import play.api.http.HttpFilters
+import play.filters.gzip.GzipFilter
 import play.api.mvc._
 
-import filters.ExampleFilter
+import filters.LoggingFilter
 
 /**
  * This class configures filters that run on every request. This
@@ -21,13 +23,15 @@ import filters.ExampleFilter
 @Singleton
 class Filters @Inject() (
   env: Environment,
-  exampleFilter: ExampleFilter) extends HttpFilters {
+  gzip: GzipFilter,
+  log: LoggingFilter,
+  cors: CORSFilter) extends HttpFilters {
 
   override val filters = {
     // Use the example filter if we're running development mode. If
     // we're running in production or test mode then don't use any
     // filters at all.
-    if (env.mode == Mode.Dev) Seq(exampleFilter) else Seq.empty
+    if (env.mode == Mode.Dev) Seq(gzip, log, cors) else Seq.empty
   }
 
 }
--- build.sbt
+++ build.sbt
@@ -9,6 +9,7 @@ scalaVersion := "2.11.8"
 libraryDependencies ++= Seq(
   cache,
   ws,
+  filters,
   "org.scalatestplus.play" %% "scalatestplus-play" % "1.5.1" % Test
 )
 
